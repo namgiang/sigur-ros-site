cb321a67db86259e80c0fd76795c39b078543f5e
namgiang <namgiang0606@gmail.com>
2017-07-18 19:20:03 +0200

    add video & image background, and add artist logo

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,26 @@
-console.log('\'Plugify \'Plugify!');
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('background--video', {
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+function onPlayerReady() {
+  player.mute();
+}
+
+function onPlayerStateChange (event) {
+  if (event.data == YT.PlayerState.PLAYING) {
+    document.querySelector('#background--video').style.opacity = 1;
+  }
+}
